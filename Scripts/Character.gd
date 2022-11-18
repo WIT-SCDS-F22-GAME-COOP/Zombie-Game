24c189ded3a307c3b6aa8343f9727f506ae951bd
brownc32atwit <brownc32@wit.edu>
2022-11-18 15:33:37 -0500

    Direct move tiles

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
 var direction = 0
+var move_direction = -1
+
 var id = 0
+
+var starting_pos = Vector2.ZERO
 var current_tile = -1
 
 # Called when the node enters the scene tree for the first time.
@@ -15,15 +19,20 @@ func _process(delta):
 	# Mapped to R button, walks
 	if Input.is_action_just_pressed("debug_input"):
 		use_tile()
-		walk()
+		if (move_direction == -1):
+			walk(direction)
+		else:
+			walk(move_direction)
+			move_direction = -1
 	# Mapped to T button, turns right
 	if Input.is_action_just_pressed("debug_input2"):
 		direction = Tiles.turn(direction, true)
 		print(current_tile)
 
+
 # Current facing direction. 0 Up, 1 right, 2 down, 3 left
-func walk():
-	match(direction):
+func walk(x):
+	match(x):
 		0:
 			global_position.y = global_position.y - 64
 		1:
@@ -33,9 +42,23 @@ func walk():
 		3:
 			global_position.x = global_position.x - 64
 
+
 func use_tile():
 	match(current_tile):
 		0:
 			direction = Tiles.turn(direction,false)
 		1:
 			direction = Tiles.turn(direction,true)
+		# UDLR
+		2:
+			move_direction = 0
+		3:
+			move_direction = 2
+		4:
+			move_direction = 3
+		5:
+			move_direction = 1
+
+
+func _on_Timer_timeout():
+	starting_pos = global_position
